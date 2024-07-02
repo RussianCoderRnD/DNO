3ca9042aeb3bf76750e6fa9f1fc7e0591e561171
RussianCoderRnD <stalkerolegator@yandex.ru>
2024-07-02 17:29:59 +0300

    прототипы хетеров define, variebles b librares

---
+++ src/libraries.h
@@ -0,0 +1,10 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <Arduino.h>          // Основная библиотека для Arduino
+
+#include <Define.h>           // Файл с инструкциями #define
+#include <variables.h>        // Файл с объявлением переменных
+
+
+#endif 
---
+++ src/variables.h
@@ -0,0 +1,6 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#include <Arduino.h> // Подключаем библиотеку Arduino
+
+#endif
